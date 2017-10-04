2707295b2c775d67f771e271c2dbfa58a27a6ae3
Isaac Aggrey <github@mail.isaacaggrey.com>
2017-10-04 14:06:39 -0500

    Use async/await instead of promise chaining

--- e2e/home.component.e2e-spec.ts
+++ e2e/home.component.e2e-spec.ts
@@ -3,16 +3,14 @@ import { HomeComponentPage } from '../src/app/home.pageobject';
 describe('Home Component', () => {
    let component = new HomeComponentPage();
 
-  it('should render', (done) => {
+  it('should render', async (done) => {
     HomeComponentPage.go();
 
-    component.headingText.then((h1result: string) => {
-      expect(h1result).toEqual('SKY UX Template');
+    let h1 = await component.headingText;
+    expect(h1).toEqual('SKY UX Template');
 
-      component.alertText.then((alertResult: string) => {
-        expect(alertResult).toEqual(`You've just taken your first step into a larger world.`);
-        done();
-      });
-    });
+    let alert  = await component.alertText;
+    expect(alert).toEqual(`You've just taken your first step into a larger world.`);
+    done();
   });
 });
